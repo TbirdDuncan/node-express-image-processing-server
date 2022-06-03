75d82af6b4d418869f1d4be6431144ed32be92ff
TristanAnderson <Tristan.Anderson@target.com>
2022-06-03 13:38:09 -0500

    first commit

---
+++ api/app.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const path = require('path')
+// const router = require('./src/router')
+
+const app = express()
+
+const pathToIndex = path.resolve(__dirname, "../client/index.html")
+app.use("/*", (request, response)=>{
+    response.sendFile(pathToIndex)
+})
+
+module.exports = app
